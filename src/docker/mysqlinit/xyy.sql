c0787071dc76ecad51072e2344ab1ce0bae12e03
yyw <953834564@qq.com>
2020-06-12 15:14:19 +0800

    docker: first write sql device

--- src/docker/mysqlinit/xyy.sql
+++ src/docker/mysqlinit/xyy.sql
@@ -1,3 +1,4 @@
+DROP DATABASE DFS;
 CREATE SCHEMA DFS;
 
 use DFS;
@@ -64,5 +65,46 @@ PRIMARY KEY (`ID`)
 CREATE UNIQUE INDEX `idx_FILE_PATH_NAME`  ON `DFS`.`FILE` (PATH, NAME) COMMENT '' ALGORITHM DEFAULT LOCK DEFAULT;
 
 CREATE UNIQUE INDEX `idx_USER_NAME`  ON `DFS`.`USER` (NAME) COMMENT '' ALGORITHM DEFAULT LOCK DEFAULT;
+
+INSERT INTO `FILE` VALUES (9, '1.txt', 'TIM/hello/', 'rw-r--r--', '20170420', 5,0, false, 'xixi','txt',0);
+
+INSERT INTO `FILE` VALUES (10, '2.txt', 'TIM/hello/', 'rw-r--r--', '20170421', 3, 0,false, 'xixi','txt',0);
+
+INSERT INTO `FILE` VALUES (11, '3', 'TIM/hello/', 'rwxr-xr-x', '20170421', 5, 0,true, 'xixi','',0);
+
+INSERT INTO `FILE` VALUES (12, '4.txt', '233/hi/', 'rw-r--r--', '20170421', 5, 0,false, 'haha','txt',0);
+
+INSERT INTO `FILE` VALUES (1, 'TIM', '/', 'rwxrwxrwx', '20170419', 0,0, true, 'xixi','',0);
+
+INSERT INTO `FILE` VALUES (4, '233', '/', 'rwxrwxrwx', '20170419', 0,0, true, 'haha','',0);
+
+INSERT INTO `FILE` VALUES (2, 'hi', '233/', 'rwxrwxrwx', '20170419', 0,0, true, 'haha','',0);
+
+INSERT INTO `FILE` VALUES (3, 'hello', 'TIM/', 'rwxrwxrwx', '20170419', 0,0, true, 'xixi','',0);
+
+INSERT INTO `FILE` VALUES (5, 'test.txt', '/', 'rwxrwxrwx', '20170419', 1,0, false, 'xixi','txt',8);
+INSERT INTO `FILE` VALUES (6, 'test.jpg', '/', 'rwxrwxrwx', '20170419', 1,0, false, 'xixi','jpg',74347);
+
+INSERT INTO FRAGMENT VALUES (500, '1');
+
+INSERT INTO FRAGMENT VALUES (600, '1');
+
+INSERT INTO `DEVICE` VALUES (1, '127.0.0.1', 9998, true, 10);
+
+INSERT INTO `DEVICE` VALUES (3, '1.2.3.4', 2111, false, 20);
+
+INSERT INTO `DEVICE` VALUES (5, '8.8.8.9', 2560, false, 21);
+
+INSERT INTO `DEVICE` VALUES (9, '11.12.13.14', 2480, true, 22);
+
+INSERT INTO `DEVICE` VALUES (11, '11.87.13.33', 5556, true, 30);
+
+INSERT INTO `DEVICE` VALUES (15, '121.37.233.322', 5012, true, 40);
+
+INSERT INTO `USER` VALUES (1, 'xixi', '123456', 'xixi');
+
+INSERT INTO `USER` VALUES (2, 'haha', '888888', 'haha');
+
+INSERT INTO `USER` VALUES (3, 'huhu', '520520', 'huhu');
 
 GRANT ALL PRIVILEGES ON *.* TO 'root'@'127.0.0.1' IDENTIFIED BY 'root';
